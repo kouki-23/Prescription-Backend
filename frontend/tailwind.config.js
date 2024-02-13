d5276e19822c8219f4e863d9a139e2682cb06179
EYA MANAI <eyamanai35@outlook.com>
2024-02-13 15:35:51 +0100

    configuration tailwind theme colors and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "primary-blue": "#233368",
+      "secondary-blue": "#0096DC",
+      "light-blue": "#699BF7",
+      "primary-gray": "#F2F2F3",
+      "secondary-gray": "#D9D9D9",
+      "dark-gray": "#CFD7E8",
+      "gray-button": "#8F8989",
+      "gray-table": "#F3F3F5",
+      "placeholder-gray": "#777171",
+      "light-black": "#04060C",
+      "white-shade": "#FFFFFF",
+      "primary-red": "#FF3A00",
+      "secondary-red": "#FF3636",
+      "orange-shade": "#FCA119",
+      "green-shade": "#00FF1A",
+    },
+    fontFamily: {
+      montserrat: ["Montserrat"],
+      lato: ["Lato"],
+    },
     extend: {},
   },
   plugins: [],
