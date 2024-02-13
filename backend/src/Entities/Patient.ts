4b1e12674041b626cfff7cf94772a62130c38a2f
Adem <adem0946chaa@gmail.com>
2024-02-13 17:34:59 +0100

    add User,DataHistory,Patient,Prescription,Protocol entities

---
+++ backend/src/Entities/Patient.ts
@@ -0,0 +1,60 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import { DataHistory } from "./DataHistory"
+import { Prescription } from "./Prescription"
+
+@Entity()
+export class Patient {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  DMI: number
+
+  @Column()
+  index: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  gender: string
+
+  @Column()
+  matrimonial: string
+
+  @Column("date")
+  BirthDate: Date
+
+  @Column("decimal")
+  weight: number
+
+  @Column("decimal")
+  height: number
+
+  @Column("decimal")
+  bodySurface: number
+
+  @Column("decimal")
+  creatinine: number
+
+  @Column()
+  clairanceFormula: string
+
+  @Column("decimal")
+  clairance: number
+
+  @Column()
+  comment: string
+
+  @Column()
+  serviceType: string
+
+  @OneToMany(() => DataHistory, (datahistory) => datahistory.patient)
+  dataHistory: DataHistory[]
+
+  @OneToMany(() => Prescription, (p) => p.patient)
+  prescription: Prescription[]
+}
