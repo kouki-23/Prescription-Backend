881b38c5fe88392d3c2e2650f1196175339eda2d
Adem <adem0946chaa@gmail.com>
2024-02-21 20:59:59 +0100

    qol improvement Login page

--- frontend/src/components/atoms/LoginBtn.tsx
+++ frontend/src/components/atoms/LoginBtn.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useRef } from "react"
+
 type Props = {
   text: string
   clickFn: React.MouseEventHandler<HTMLButtonElement>
 }
 
 export default function LoginBtn({ text, clickFn }: Props) {
+  const btn = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault()
+        btn.current?.click()
+      }
+    }
+    document.addEventListener("keydown", listener)
+    return () => {
+      document.removeEventListener("keydown", listener)
+    }
+  }, [])
   return (
     <button
       className="bg-primary-blue bg-opacity-90 rounded-md px-28 py-2 text-white-shade text-2xl font-semibold hover:bg-opacity-100"
       onClick={clickFn}
+      ref={btn}
     >
       {text}
     </button>
