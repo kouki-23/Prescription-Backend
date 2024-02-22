3975caabcb2b3f68416280c635d423f43d50dc2a
Adem <adem0946chaa@gmail.com>
2024-02-22 10:16:51 +0100

    index now redirect you to the corresponding route

--- frontend/src/pages/Index/Index.tsx
+++ frontend/src/pages/Index/Index.tsx
@@ -1,11 +1,18 @@
-import Header from "../../components/molecules/Header"
+import { Navigate } from "react-router-dom"
+import { useAuth } from "../../helpers/auth/auth"
+import ErrorPage from "../Error/ErrorPage"
 
 type Props = {}
 
 export default function Index({}: Props) {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+  const { user } = useAuth()
+  if (user?.role === "medecin") {
+    return <Navigate to={"/medecin"} />
+  } else if (user?.role === "pharmacien") {
+    return <Navigate to={"/pharmacien"} />
+  } else if (user?.role === "admin") {
+    return <Navigate to={"/admin"} />
+  } else {
+    return <ErrorPage cause="unkown role" />
+  }
 }
