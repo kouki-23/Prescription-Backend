2bd47a718621d33d57f31c71a900f7ac18758c13
Adem <adem0946chaa@gmail.com>
2024-02-23 19:31:07 +0100

    move componemts to correct place

--- frontend/src/components/molecules/LabledInput.tsx
+++ frontend/src/components/molecules/LabledInput.tsx
@@ -1,19 +1,16 @@
+import TextInput from "../atoms/TextInput"
+
 type Props = {
   text: string
   value: string
-  setValue: Function
+  setValue: React.Dispatch<React.SetStateAction<string>>
 }
 
 export default function LabledInput({ text, value, setValue }: Props) {
   return (
     <div>
       <label className="block mb-0">{text}</label>
-      <input
-        type="text"
-        className="bg-primary-gray rounded-lg h-7 px-2 focus:outline-secondary-blue"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      <TextInput value={value} setValue={setValue} />
     </div>
   )
 }
