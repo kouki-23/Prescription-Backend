19a36089ebddafa6273209887301f00570980269
EYA MANAI <eyamanai35@outlook.com>
2024-02-26 11:30:04 +0100

    Molecule CRUD

--- backend/src/Handlers/molecule.ts
+++ backend/src/Handlers/molecule.ts
@@ -1,8 +1,19 @@
 import { Request, NextFunction } from "express"
-import { CreateMoleculeBody } from "../Middlewares/validation/schema"
-import { createMolecule, getAllMolecules } from "../Services/moleculeService"
+import {
+  CreateMoleculeBody,
+  MoleculeByIdParams,
+  UpdateMoleculeBody,
+} from "../Middlewares/validation/schema"
+import {
+  UpdateMolecule,
+  createMolecule,
+  deleteMolecule,
+  getAllMolecules,
+  getMoleculeById,
+} from "../Services/moleculeService"
 import { Response } from "express"
 import { HttpError, StatusCode } from "../Utils/HttpError"
+import { nextTick } from "process"
 
 export async function createMoleculeHandler(
   req: Request<never, never, CreateMoleculeBody, never>,
@@ -37,5 +48,55 @@ export async function getAllMoleculesHandler(
         StatusCode.InternalServerError,
       ),
     )
+  }
+}
+
+export async function getMoleculeByIdHandler(
+  req: Request<MoleculeByIdParams, never, never, never>,
+  res: Response,
+  next: NextFunction,
+) {
+  const { id } = req.params
+  try {
+    const molecule = await getMoleculeById(Number(id))
+    res.json(molecule)
+  } catch (e) {
+    next(e)
+  }
+}
+
+export async function UpdateMoleculeBodyHandler(
+  req: Request<MoleculeByIdParams, never, UpdateMoleculeBody, never>,
+  res: Response,
+  next: NextFunction,
+) {
+  const { id } = req.params
+  try {
+    const isUpdated = await UpdateMolecule(Number(id), req.body)
+    if (isUpdated) {
+      res.sendStatus(200)
+    } else {
+      next(new HttpError("No molecule Updated", StatusCode.BadRequest))
+    }
+  } catch (e) {
+    next(e)
+  }
+}
+
+export async function DeleteMoleculeHandler(
+  req: Request<MoleculeByIdParams, never, never, never>,
+  res: Response,
+  next: NextFunction,
+) {
+  const { id } = req.params
+  try {
+    const isdeleted = await deleteMolecule(Number(id))
+    if (isdeleted) {
+      res.sendStatus(200)
+    } else {
+      next(new HttpError("no molecule is deleted", StatusCode.BadRequest))
+    }
+  } catch (e) {
+    next(e)
   }
 }
