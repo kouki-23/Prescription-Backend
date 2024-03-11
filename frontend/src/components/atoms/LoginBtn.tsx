c04a84ef4fc4763d88220f18cd29a47338bd47a4
EYA MANAI <eyamanai35@outlook.com>
2024-03-11 20:01:22 +0100

    redesign Log in

--- frontend/src/components/atoms/LoginBtn.tsx
+++ frontend/src/components/atoms/LoginBtn.tsx
@@ -21,7 +21,7 @@ export default function LoginBtn({ text, clickFn }: Props) {
   }, [])
   return (
     <button
-      className="bg-primary-blue bg-opacity-90 rounded-md px-28 py-2 text-white-shade text-2xl font-semibold hover:bg-opacity-100"
+      className="bg-primary-blue bg-opacity-90 rounded-md px-20 py-2 text-white-shade text-2xl font-semibold hover:bg-opacity-100"
       onClick={clickFn}
       ref={btn}
     >
