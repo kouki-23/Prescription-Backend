2e317643a680fb9ac9496f23922e37d6bb009904
EYA MANAI <eyamanai35@outlook.com>
2024-04-16 14:54:02 +0200

    speciality list development

--- frontend/src/pages/Admin/SpecialityList.tsx
+++ frontend/src/pages/Admin/SpecialityList.tsx
@@ -1,26 +1,35 @@
 import Title from "@components/atoms/Title"
-import SideNavbar from "@components/organisms/SideNavbar"
 import { SpecialtityTable } from "@components/organisms/SpecialityTable"
 import { useNavigate } from "react-router-dom"
 import addIcon from "@assets/icons/add.svg"
+import { useQuery } from "@tanstack/react-query"
+import { getAllProducts } from "@helpers/apis/product"
+import { SearchInput } from "@components/atoms/SearchInput"
+import LoadingInterface from "@components/organisms/LoadingInterface"
+import ErrorPage from "@pages/Error/ErrorPage"
+
 type Props = {}
 export default function SpecialityList({}: Props) {
   const navigate = useNavigate()
-
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["products"],
+    queryFn: getAllProducts,
+  })
+  if (isLoading) return <LoadingInterface />
+  if (error) return <ErrorPage cause={error.message} />
   return (
-    <div className="flex">
-      <SideNavbar />
-      <div className="w-4/5 px-12">
-        <div className="container mx-auto my-8 mt-32 flex justify-between ">
-          <Title text="Liste spécialité " />
-          <img
-            className=" size-10 cursor-pointer  "
-            src={addIcon}
-            alt="ajouter specialité"
-          />
-        </div>
-        <SpecialtityTable />
+    <div>
+      <SearchInput placeholder="Rechercher specialité" />
+      <div className="container mx-auto my-8 mt-32 flex justify-between ">
+        <Title text="Liste spécialité " />
+        <img
+          className=" size-10 cursor-pointer  "
+          src={addIcon}
+          onClick={() => navigate("/admin/ajouterSpecialite")}
+          alt="ajouter specialité"
+        />
       </div>
+      <SpecialtityTable data={data!} refetch={refetch} />
     </div>
   )
 }
