09b32b91dcfb8a05e7f861287a05ca74072c8299
EYA MANAI <eyamanai35@outlook.com>
2024-04-16 14:53:15 +0200

    product api

--- frontend/src/helpers/apis/product.ts
+++ frontend/src/helpers/apis/product.ts
@@ -1,1 +1,12 @@
 import axios from "axios"
+import { Product } from "@helpers/types"
+export async function getAllProducts() {
+  const response = await axios.get<Product[]>("/product/")
+  return response.data
+}
+export async function addProduct(data: any) {
+  await axios.post("/product/", data)
+}
+export async function deleteProduct(id: number) {
+  await axios.delete(`/product/${id}`)
+}
