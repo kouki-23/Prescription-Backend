e3bf20999155ac7ecf205181831211757ff785c6
EYA MANAI <eyamanai35@outlook.com>
2024-04-16 14:51:59 +0200

    search input atoms frontend

---
+++ frontend/src/components/atoms/SearchInput.tsx
@@ -0,0 +1,23 @@
+import searchIcon from "@assets/icons/search.svg"
+export function SearchInput({
+  placeholder,
+  className,
+}: {
+  className?: string
+  placeholder?: string
+}) {
+  return (
+    <label>
+      <div className=" relative mt-2 ">
+        <input
+          type="search"
+          className="border rounded-lg bg-gray-table border-none h-8 w-80 focus:outline-none shadow-md pl-8"
+          placeholder={placeholder}
+        />
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3">
+          <img src={searchIcon} className="size-4" />
+        </div>
+      </div>
+    </label>
+  )
+}
